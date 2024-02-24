f19723174d077b6d4351fbfd3eb0cfcfdb48ec30
URLbug <gemslove2@gmail.com>
2024-02-24 15:39:23 +0200

    Add func_form

---
+++ users/func_form/ReportForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace App.users.func_form
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            descriptionRep = new TextBox();
+            publickButton = new Button();
+            exitButton = new Button();
+            SuspendLayout();
+            // 
+            // descriptionRep
+            // 
+            descriptionRep.Location = new Point(12, 22);
+            descriptionRep.Multiline = true;
+            descriptionRep.Name = "descriptionRep";
+            descriptionRep.Size = new Size(273, 232);
+            descriptionRep.TabIndex = 0;
+            // 
+            // publickButton
+            // 
+            publickButton.Location = new Point(64, 278);
+            publickButton.Name = "publickButton";
+            publickButton.Size = new Size(168, 23);
+            publickButton.TabIndex = 1;
+            publickButton.Text = "Опубликовать";
+            publickButton.UseVisualStyleBackColor = true;
+            publickButton.Click += publickButton_Click;
+            // 
+            // exitButton
+            // 
+            exitButton.Location = new Point(64, 307);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(168, 23);
+            exitButton.TabIndex = 2;
+            exitButton.Text = "Выйти";
+            exitButton.UseVisualStyleBackColor = true;
+            exitButton.Click += exitButton_Click;
+            // 
+            // ReportForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(297, 344);
+            Controls.Add(exitButton);
+            Controls.Add(publickButton);
+            Controls.Add(descriptionRep);
+            Name = "ReportForm";
+            Text = "ReportForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox descriptionRep;
+        private Button publickButton;
+        private Button exitButton;
+    }
+}
---
+++ users/func_form/ReportForm.cs
@@ -0,0 +1,34 @@
+﻿using App.components.database;
+using App.components.jsons;
+
+namespace App.users.func_form
+{
+    public partial class ReportForm : Form
+    {
+        Database db = new Database();
+        public ReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void publickButton_Click(object sender, EventArgs e)
+        {
+            string text = descriptionRep.Text;
+
+            Sessions sessions = new Sessions();
+
+            UserSession json = sessions.JsonRead<UserSession>(@"./user.json");
+
+            db.Add("reports", $"({json.Id}, '{text}')");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            MainForm main = new MainForm();
+
+            main.Show();
+
+            this.Close();
+        }
+    }
+}
